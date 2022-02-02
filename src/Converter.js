3a49695350228fcdd441792206139a84b50030b6
Joao Albuquerque <joaopealbuquerque1@gmail.com>
2022-02-01 21:35:55 -0300

    Adicionando estrutura html aguardando ajustes

--- src/Converter.js
+++ src/Converter.js
@@ -1,9 +1,49 @@
 import './Converter.css';
+import {Button, Form,Row, Col, Spinner} from 'react-bootstrap'
+// import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+// import {faAngleDoubleRight} from '@fortawesome/free-solid-svg-icons'
 
-export function CurrencyConverter() {
+function CurrencyConverter() {
   return (
-    <h1>Hello Word</h1>
+    <div>
+      <h1>Currency Converter</h1>
+      <Form>
+        <Row>
+          <Col sm="3">
+            <Form.Control></Form.Control>
+          </Col>
+        </Row>
+      </Form>
+    </div>
+  //   <div>
+  //   <h1>Currency Converter</h1>
+  //   <Form>
+  //     <Form.Row>
+  //       <Col sm="3">
+  //         <Form.Control placeholder='0' value={1} required/>
+  //       </Col>
+  //       <Col sm='3'>
+  //         <Form.Control as="select">
+
+  //         </Form.Control>
+  //       </Col>
+  //       {/* <Col sm='1' className='text-center' style={{paddingTop:'5px'}}>
+  //       <FontAwesomeIcon icon={faAngleDoubleRight} />
+  //       </Col> */}
+  //       <Col sm='3'>
+  //         <Form.Control as="select">
+            
+  //         </Form.Control>
+  //       </Col>
+  //       <Col sm='2'>
+  //         <Button variant='success' type='submit'>
+  //           Converter
+  //         </Button>
+  //       </Col>
+  //     </Form.Row>
+  //   </Form>
+  // </div>
   );
 }
 
-
+export default CurrencyConverter
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {CurrencyConverter}  from './Converter';
+import CurrencyConverter  from "./Converter";
 import './index.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 ReactDOM.render(
   <React.StrictMode>
-    <CurrencyConverter />
+    <CurrencyConverter/>
   </React.StrictMode>,
   document.getElementById('root')
 );
